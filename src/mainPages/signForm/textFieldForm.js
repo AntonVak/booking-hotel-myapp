c460b52bd416f5fbcd180c510b61b9dd07a5f98a
AntonVak <antonv.snab@gmail.com>
2023-04-13 17:21:31 +0300

    add button & logic  showPassword

--- src/mainPages/signForm/textFieldForm.js
+++ src/mainPages/signForm/textFieldForm.js
@@ -1,20 +1,40 @@
+import { useState } from "react";
+
 const TextFieldForm = ({ label, type, name, value, onChange, error }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   const getInputClass = () => {
-    return "form-control" + (error ? " is-invalid" : "")
-  }
+    return "form-control" + (error ? " is-invalid" : "");
+  };
+
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
   return (
     <div className="mb-4">
       <label htmlFor={name}>{label}</label>
-      <input
-        type={type}
-        id={name}
-        value={value}
-        onChange={onChange}
-        name={name}
-        // autoComplete={autoComplete}
-        className={getInputClass()}
-      />
-      {error && <div className="invalid-feedback">{error}</div>}
+      <div className="input-group has-validation">
+        <input
+          type={showPassword ? "text" : type}
+          id={name}
+          value={value}
+          onChange={onChange}
+          name={name}
+          // autoComplete={autoComplete}
+          className={getInputClass()}
+        />
+        {type === "password" && (
+          <button
+          className="btn btn-outline-secondary"
+          type="button"
+          onClick={toggleShowPassword}
+        >
+          <i className={"bi bi-eye" + (showPassword ? "-slash" : "")}></i>
+        </button>
+        )}
+        {error && <div className="invalid-feedback">{error}</div>}
+      </div>
+      
     </div>
   );
 };
