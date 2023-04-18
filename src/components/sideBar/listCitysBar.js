3235c0057a220cabd7db57539f9d4daf24c52d50
AntonVak <antonv.snab@gmail.com>
2023-04-18 11:00:44 +0300

    add comp listCitysBar

---
+++ src/components/sideBar/listCitysBar.js
@@ -0,0 +1,17 @@
+const ListCitysBar = ({hotels, city}) => {
+    console.log(hotels)
+  return (
+    <ul className="list-group">
+    {hotels && hotels.map((hotel) => (
+        <li className="list-group-item" id={hotel.id}>{hotel.city}</li>
+    ))}
+      <li className="list-group-item"></li>
+      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A third item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth one</li>
+    </ul>
+  );
+};
+
+export default ListCitysBar;
